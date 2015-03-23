8255e8740a4ba1b5f64bcdfd067823db83b31b46
Nozel Rosario <nozelrosario@gmail.com>
2015-03-24 03:12:33 +0530

    Implemented FB Integration.

--- controllers/main/bannerController.js
+++ controllers/main/bannerController.js
@@ -1,6 +1,6 @@
 angular.module('home.banner', [])
 
-.controller('bannerController', function ($scope, $state) {
+.controller('bannerController', function ($scope, $state, ezfbProvider) {
     // alert('banner Controller');
     $scope.doSearch = function () {
         $state.go("main.search", { "key": $scope.searchKeyword });
--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
-// Ionic D-Care App
+// Globar Constants
+//https://help.yahoo.com/kb/yahoo-merchant-solutions/facebook-application-sln18861.html
+var FB_APP_ID = '1608106426069055';  // Nozel FB App ID registered for : http:\\developer.nozel.com
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 
 var phulrani = angular.module('phulrani', ['ui.router', 'ui.bootstrap', 'ngAnimate', //'ngMaterial',
 											'home.banner', 'home.footer', 'home.homePage', 'home.navigation',
-                                            'home.productsFilter'
+                                            'home.productsFilter', 'ezfb'
 										  ]);
 
 /*
 * App Config 
 */
-phulrani.config(function ($logProvider) {
+phulrani.config(function ($logProvider, ezfbProvider) {
     $logProvider.debugEnabled(true);
+    // FB Settings : 
+    ezfbProvider.setInitParams({
+        appId: FB_APP_ID
+    });
 });
 
 /*
